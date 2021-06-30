853bc17105eb6bbc631583e9ce18568d1114158c
Rakesh Ravuri <rravuri@gmail.com>
2021-06-30 17:42:12 +0530

    use # for private

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,43 +1,58 @@
-export default class User {
-  readonly id: string;
-  name: string|null;
-  private _email: string;
-  private _details?: any={};
-  private _roles: string[]=[];
+import { IModel } from "../common";
 
-  constructor(data:{id:string, email:string, name?:string, details?:object}){
-    this.id = data.id;
+export default class User implements IModel {
+  readonly #id: string;
+  #name: string|null;
+  #email: string;
+  #details?: object={};
+  #roles: string[]=[];
+
+  constructor(data:{id:string, email:string, name?:string, details?:object, roles?:string[]}){
+    this.#id = data.id;
     this.email = data.email,
-    this.name = data.name||null;
-    this.details = data.details;
+    this.#name = data.name||null;
+    this.details = data.details||{};
+    this.#roles = data.roles||[];
+  }
+
+  get id():string {
+    return this.#id;
+  }
+
+  get name():string|null {
+    return this.#name
+  }
+  
+  set name(name:string|null) {
+    this.#name=name;
   }
 
   get email():string {
-    return this._email;
+    return this.#email;
   }
   set email(email:string) {
-    this._email = email.toLocaleLowerCase();
+    this.#email = email.toLocaleLowerCase();
   }
 
-  get details():any {
-    return this._details;
+  get details():object {
+    return this.#details;
   }
-  set details(details:any) {
-    this._details = details||{};
+  set details(details:object) {
+    this.#details = details||{};
   }
 
   get roles():ReadonlyArray<string> {
-    return this._roles;
+    return this.#roles;
   }
 
 
-  toJSON(): object {
+  toJSON():object {
     return {
       id: this.id,
       email: this.email,
       name: this.name,
       details: this.details,
-      roles: this._roles
+      roles: this.roles
     }
   }
 
